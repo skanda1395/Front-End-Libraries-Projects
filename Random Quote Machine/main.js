93e4a5cb50be7dffbd92c49b6ceb8651395c653e
Sunil Skanda <skanda1395@gmail.com>
2020-04-30 15:32:53 +0530

    added first draft of first project

---
+++ Random Quote Machine/main.js
@@ -0,0 +1,31 @@
+let app = new Vue({
+  el: "#app",
+  data: {
+    quote: "",
+    author: "",
+  },
+  methods: {
+    getQuote() {
+      fetch("https://programming-quotes-api.herokuapp.com/quotes/random")
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Network response was not ok");
+          }
+          return response.json();
+        })
+        .then((data) => {
+          this.quote = data.en;
+          this.author = data.author;
+        })
+        .catch((error) => {
+          console.error(
+            "There has been a problem with your fetch operation:",
+            error
+          );
+        });
+    },
+  },
+  mounted: function () {
+    this.getQuote();
+  },
+});
